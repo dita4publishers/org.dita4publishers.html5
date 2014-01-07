04b4adb0262ff305f0141b3db84bb7b4d923d445
Bertrand Lefort <blefort@uottawa.ca>
2014-01-07 15:14:07 -0500

    add foundation icons fonts to replace ui-icons, more versatile

--- html5-libs/modules/jQuery-widgets/navigation/classic/js/navigation.js
+++ html5-libs/modules/jQuery-widgets/navigation/classic/js/navigation.js
@@ -14,108 +14,199 @@
  *
  */
 var navigation = {
+  // navigation object
+  o:{},
 
-  'icon': 'ui-icon',
-
-  'leaf': 'ui-icon-triangle-1-e',
-
-  'leafActive': 'ui-icon-triangle-1-s',
-
-  'toolbar': {
-    'id': 'navToolBar',
-    'position': 'top'
+  // navigation information, ex. selector
+  nav: {
+    id: '#local-navigation'
   },
 
-  'buttons': false,
+  // class
+  cs: {
+    leaf: 'leaf',
+    expand: 'fi-arrows-expand',
+    collapse: 'fi-arrows-compress',
+    close: 'fi-x-circle'
+  },
 
+  // text value
+  text: {
+    expand: 'Expand All',
+    collapse: 'Collapse All',
+    close: 'Close Menu'
+  },
+
+  // toolbar information
+  toolbar: {
+    id: 'navToolBar',
+    position: 'top',
+    o: {}
+  },
+
+  // mobile toolbar information
+  mobileToolbar: {
+    sel: '.toggle-topbar a'
+  },
+
+  // buttons information
+  buttons: {
+    collapse: 'collapseBtn',
+    expand: 'expandBtn',
+    close: 'closeBtn'
+  },
+
+  // tell is the navigation is collapsed in mobile view
+  collapsed: true,
+
+  // animation transition speed
+  transitionSpeed: 100,
+
+  // traverse the navigation and add required semantic
   traverse: function () {
 
-    var o = this;
+    var self = this;
 
-    $("#local-navigation" + ' li')
-      .each(function (index) {
+    this.o.find('li').each(function (index) {
+      var span = {}, span2 = {};
 
-         var span = {}, span2 = {};
+      $(this).attr('role', 'treeitem');
 
-         $(this).attr('role', 'treeitem');
+      // collapsible
+      if ($(this).find('ul').length) {
 
-         // collapsible
-         if ($(this).hasClass('collapsible')) {
+        // create span for icone
+        span = $("<span/>").html(' ');
+        span.addClass(self.cs.leaf);
+        span.on('click', function() {
+          $(this).parent().toggleClass('active').toggleClass('collapsed');
+        });
 
-            // create span for icone
-            span = $("<span/>");
-            span.addClass(o.icon);
-            span.addClass($(this).hasClass('active') ? o.leafActive : o.leaf);
-            span.on('click', function() {
-              $(this).parent().toggleClass('active').toggleClass('collapsed');
-            });
-            $(this).prepend(span);
-          }
-          //topichead
-          $(this).children('.navtitle').on('click', function() {
-              $(this).parent().toggleClass('active').toggleClass('collapsed');
-          });
-        }
-      );
-    },
-
-    addToolbar: function () {
-      if(this.toolbar.position == 'top') {
-         $("#local-navigation").prepend($("<div />").attr('id', this.toolbar.id).attr('class', 'toolbar top hide-on-small'));
-      } else {
-        $("#local-navigation").append($("<div />").attr('id', this.toolbar.id).attr('class', 'toolbar bottom hide-on-small'));
+        $(this).prepend(span);
       }
-    },
 
-    addButtons: function () {
-      var o = this,
-      expandIco = $("<span/>").attr("class", "ui-icon ui-icon-carat-2-n-s"),
-      collapseIco = $("<span/>").attr("class", "ui-icon ui-icon-carat-2-n"),
-      expand = $("<span/>").attr("class", "hidden").html("Expand All"),
-      collapse = $("<span/>").attr("class", "hidden").html("Collapse All"),
-      btnExpand = $("<button/>").attr('id', 'expandBtn').attr('class', 'ui-state-default').append(expandIco).append(expand).click(function(){
-        $("#local-navigation" + ' li')
-          .addClass('active')
-          .removeClass('collapsed')
-          .attr('aria-expanded', 'true');
-
-           $(this).hide();
-           $('#collapseBtn').show();
-
-        }),
-      btnCollapse = $("<button/>").attr('id', 'collapseBtn').attr('class', 'ui-state-default').append(collapseIco).append(collapse).hide().click(function(){
-        o.select();
-        $("#local-navigation" + ' li')
-          .removeClass('selected')
-          .removeClass('active')
-          .addClass('collapsed')
-          .removeAttr('aria-expanded');
-
-           o.select();
-           $(this).hide();
-           $('#expandBtn').show();
+      //topichead
+      $(this).children('.navtitle').on('click', function() {
+         $(this).parent().toggleClass('active').toggleClass('collapsed');
       });
+    });
+  },
 
-      $('#'+this.toolbar.id).append(btnExpand);
-      $('#'+this.toolbar.id).append(btnCollapse);
-    },
+  addToolbar: function () {
+    this.toolbar.o = $('<div />').attr('id', this.toolbar.id).attr('class', 'toolbar hide-on-small' + this.toolbar.position)
+    this.o.prepend(this.toolbar.o);
+  },
 
-    init: function (fn) {
+  addButtons: function () {
+    var self = this,
+    expandIco = $("<span/>").attr("class", this.cs.expand),
+    collapseIco = $("<span/>").attr("class", this.cs.collapse),
+    closeIco = $("<span/>").attr("class", this.cs.close),
+    expand = $("<span/>").attr("class", "hidden").html(this.text.expand),
+    collapse = $("<span/>").attr("class", "hidden").html(this.text.collapse),
+    close = $("<span/>").attr("class", "hidden").html(this.text.close),
+    btnExpand = $("<button/>").attr('id', this.buttons.expand),
+    btnCollapse = $("<button/>").attr('id', this.buttons.collapse).hide(),
+    btnClose= $("<button/>").attr('id', this.buttons.close);
 
-      $("#local-navigation" + " > ul").attr('role', 'tree');
-      $("#local-navigation" + " li ul").attr('role', 'group');
+    btnExpand.append(expandIco);
+    btnExpand.append(expand);
+    btnExpand.on('click tap',
+     function(){
+        self.o.find('li').addClass('active').removeClass('collapsed').attr('aria-expanded', 'true');
+           $(this).hide();
+           $('#'+self.buttons.collapse).show();
+        }
+    );
 
-      this.traverse();
-
-      this.addToolbar();
-      this.addButtons();
-
-
-    }
-    };
-
-    $(function() {
-      navigation.init();
+    btnCollapse.append(collapse);
+    btnCollapse.append(collapseIco);
+    btnCollapse.click(function(){
+      o.select();
+      self.o.find('li')
+        .removeClass('selected')
+        .removeClass('active')
+        .addClass('collapsed')
+        .removeAttr('aria-expanded');
+         o.select();
+         $(this).hide();
+         $('#'+self.buttons.expand).show();
     });
 
+    btnClose.attr('class', 'show-on-small right').append(closeIco).append(close).on('click tap',
+      function(){
+        self.menuCollapse();
+    });
+
+    $('#'+this.toolbar.id).append(btnExpand);
+    $('#'+this.toolbar.id).append(btnCollapse);
+    $('#'+this.toolbar.id).append(btnClose);
+  },
+
+    setButtonClickEvt: function ()
+    {
+      var self = this;
+      $(this.mobileToolbar.sel).on('click', function(){
+        if(self.collapsed)
+        {
+          self.o.animate(
+            {
+              left: 0
+            },
+            this.transitionSpeed,
+            function()
+            {
+              self.collapsed = false;
+            }
+          );
+        }
+      });
+    },
+
+    setCloseEvt: function ()
+    {
+      var self = this;
+      $(document).on('mouseup tap', function (e)
+      {
+        if (!self.o.is(e.target) // if the target of the click isn't the container...
+          && self.o.has(e.target).length === 0
+          && !self.collapsed) // ... nor a descendant of the container
+        {
+           self.menuCollapse();
+           self.collapsed = true;
+        }
+    });
+  },
+
+  menuCollapse: function()
+  {
+    var self = this;
+    this.o.animate(
+      {
+        left: '-100%'
+      },
+      this.transitionSpeed,
+      function() {
+        self.collapsed = true;
+      }
+    );
+  },
+
+
+  init: function () {
+    this.o = $(this.nav.id);
+    this.o.children('ul').attr('role', 'tree');
+    this.o.find('ul').attr('role', 'group');
+
+    this.traverse();
+    this.addToolbar();
+    this.addButtons();
+    this.setButtonClickEvt();
+    this.setCloseEvt();
+  }
+};
+
+$(function() {
+  navigation.init();
+});
 
